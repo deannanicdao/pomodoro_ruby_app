a8995b9d029f1a4cfe738b37f33794bb72a7031c
deannanicdao <deanna.nicdao@gmail.com>
2020-10-02 16:15:25 +1000

    Fixed small bug where user was being placed as task title. Switched input so that user was inserted last. Correctly formatted now.

--- src/task_manager.rb
+++ src/task_manager.rb
@@ -22,7 +22,7 @@ option = gets.chomp
 # === Task methods === #
 case option
 when "1"    # ADDING NEW TASKS
-    required_details = ["User", "Title", "Note"]
+    required_details = ["Title", "Note", "User"]
 
     def task_card(details)
         loop do
@@ -50,7 +50,7 @@ when "1"    # ADDING NEW TASKS
 
     if !File.exists?("test.csv")
         CSV.open("tasks.csv", "w", headers: true) do |csv|
-                csv << ["User", "Title", "Note", "Status"]
+                csv << ["Title", "Note", "Status", "User"]
         end
         task_card(required_details)
     else
@@ -73,9 +73,48 @@ when "2"    # VIEWING TASK LIST
     end
 
 when "3"    # UPDATING TASKS
+    if File.exists?("test.csv")
+        puts "What task would you like to edit?"
+        input = gets.chomp.to_i
+            # raise error if not integer
+
+        # Opening existing csv and selecting row
+        selected_row = CSV.read("test.csv")[input]
+        headers = CSV.read("test.csv")[0]
+        remaining_rows = CSV.read("test.csv")
+        
+        # Deleting selected row and headers
+        remaining_rows.delete(selected_row) # Removing a row
+        remaining_rows.delete(headers)    # Removing headers
+        
+        # Writing back to csv
+        CSV.open("test.csv", "w", headers: true) do |csv|
+            csv << ["id", "title", "note", "user", "status"]
+        end
+        
+        CSV.open("test.csv", "a", col_sep: ",", headers: true, header_converters: :symbol, skip_blanks: false) do |csv|
+            remaining_rows.each do |array|
+                csv << array
+            end
+        end
+    else
+        puts "No tasks yet."
+    end
     
 when "4"    # DELETING TASKS #
     if File.exists?("test.csv")
+        # VIEWING TASK
+        task_list = []
+        index = 0
+        CSV.foreach("test.csv", col_sep: ",", headers: true, header_converters: :symbol, skip_blanks: true) do |row|
+            task_list << Task.new(row[:user], row[:title], row[:note], row[:status])
+        end
+        task_list.each do |task|
+            index += 1
+            puts "#{index}. User: #{task.user.upcase} | #{task.title.upcase}: #{task.note} | Status: #{task.status.upcase}"
+        end
+        # END VIEWING TASK
+
         puts "What task would you like to delete?"
         input = gets.chomp.to_i
             # raise error if not integer
@@ -90,15 +129,28 @@ when "4"    # DELETING TASKS #
         remaining_rows.delete(headers)    # Removing headers
         
         # Writing back to csv
-        CSV.open("test_2.csv", "w", headers: true) do |csv|
+        CSV.open("test.csv", "w", headers: true) do |csv|
             csv << ["id", "title", "note", "user", "status"]
         end
         
-        CSV.open("test_2.csv", "a", col_sep: ",", headers: true, header_converters: :symbol, skip_blanks: false) do |csv|
+        CSV.open("test.csv", "a", col_sep: ",", headers: true, header_converters: :symbol, skip_blanks: false) do |csv|
             remaining_rows.each do |array|
                 csv << array
             end
         end
+
+        # VIEWING TASK
+        task_list = []
+        index = 0
+        CSV.foreach("test.csv", col_sep: ",", headers: true, header_converters: :symbol, skip_blanks: true) do |row|
+            task_list << Task.new(row[:user], row[:title], row[:note], row[:status])
+        end
+        task_list.each do |task|
+            index += 1
+            puts "#{index}. User: #{task.user.upcase} | #{task.title.upcase}: #{task.note} | Status: #{task.status.upcase}"
+        end
+        # END VIEWING TASK
+
     else
         puts "No tasks yet."
     end
